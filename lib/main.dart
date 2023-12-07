b65976ac1c7037754a432a832e0dfa470d198d51
Tina <x78350@gmail.com>
2023-12-07 09:40:38 +0800

    init timeapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,17 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
 
 void main() => runApp(MaterialApp(
-      home: QuoteList(),
-    ));
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({super.key});
-
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote(
-        author: 'Osca Wilde',
-        text: 'Be yourself; everyone else is already taken'),
-    Quote(
-        author: 'Osca Wilde',
-        text: 'I have nothing to declare except my genius'),
-    Quote(
-        author: 'Osca Wilde',
-        text: 'The truth is rarely pure and never simple'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-          title: const Text('Awesome Quotes'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
           centerTitle: true,
-          backgroundColor: Colors.redAccent),
-      body: Column(
-        children: quotes
-            .map((quote) => QuoteCard(
-                quote: quote,
-                delete: () {
-                  setState(() {
-                    quotes.remove(quote);
-                  });
-                }))
-            .toList(),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('Click'),
+        ),
       ),
-    );
-  }
-}
+    ));
