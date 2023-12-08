81ac7fb1a91ee7dadc09ca07314032466ddf9963
Tina <x78350@gmail.com>
2023-12-08 15:02:57 +0800

    add: Navigator.pushReplacementNamed 的用法

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -9,16 +9,17 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  String time = 'loading';
-
   void setupWorldTime() async {
     WorldTime instance = WorldTime(
         location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    print(instance.time);
-    setState(() {
-      time = instance.time;
-    });
+    if (mounted) {
+      Navigator.pushReplacementNamed(context, '/home', arguments: {
+        'location': instance.location,
+        'flag': instance.flag,
+        'time': instance.time,
+      });
+    }
   }
 
   @override
@@ -29,10 +30,10 @@ class _LoadingState extends State<Loading> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
         body: Padding(
       padding: const EdgeInsets.all(50.0),
-      child: Text(time),
+      child: Text('loading'),
     ));
   }
 }
