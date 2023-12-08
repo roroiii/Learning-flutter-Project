4f993b090e5a91478ce7d63feccd4b2eb0e70798
Tina <x78350@gmail.com>
2023-12-08 16:53:49 +0800

    add: 時間樣式與 Loading 動畫

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:learningdart/services/world_time.dart';
+import 'package:learningdart/pages/choose_location.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -31,9 +33,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(50.0),
-      child: Text('loading'),
-    ));
+        backgroundColor: Colors.blue,
+        body: Center(
+          child: SpinKitPumpingHeart(
+            color: Colors.white,
+            size: 50.0,
+          ),
+        ));
   }
 }
